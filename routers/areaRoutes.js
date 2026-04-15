963faedb7cc70e541c8782dcbab2a5312993c6d1
ssjisan <ssjisan.dev@gmail.com>
2026-04-15 15:03:13 +0600

    ROle added

--- routers/areaRoutes.js
+++ routers/areaRoutes.js
@@ -11,16 +11,23 @@ const {
 
 // Memory storage for image uploads
 const upload = require("../middlewares/upload.js");
+const { checkPermission } = require("../middlewares/checkPermission.js");
 
 // Create or update area (image is optional)
 router.post(
   "/save-area",
   requiredSignIn,
   upload("coverage-area").single("file"),
+  checkPermission("add-coverage", "canCreate"),
   createOrUpdateArea,
 );
 router.get("/areas", listAreas);
-router.delete("/area/:areaId", requiredSignIn, deleteArea);
+router.delete(
+  "/area/:areaId",
+  requiredSignIn,
+  checkPermission("coverage-list", "canDelete"),
+  deleteArea,
+);
 router.get("/area/:areaId", readArea);
 
 module.exports = router;
